4b36205b1f3addf9988e08615b6e3ba9183ef9f0
Nathur64 <36383868+Nathur64@users.noreply.github.com>
2019-03-03 05:50:54 -0600

    Create lang.kt

---
+++ src/Kotlin1/lang.kt
@@ -0,0 +1,33 @@
+package Kotlin1;
+class Quadruple constructor(first :Any, second :Any, third :Any, forth :Any){
+    var first= first;
+    var second= second;
+    var third= third;
+    var forth= forth;
+    override open fun toString() :String{ return "($first, $second, $third, $forth)"; };
+    open fun copy() :Quadruple= Quadruple(this.first, this.second, this.third, this.forth);
+    open fun iadd(o :Quadruple){
+        this.first= this.first.toLong() + o.first.toLong();
+    };
+};
+fun gType(Type :Any) :String{
+    return when(Type.javaClass){
+        Byte.javaClass->   "Byt";
+        Char.javaClass->   "Chr";
+        Int.javaClass->    "i32";
+        Long.javaClass->   "i64";
+        String.javaClass-> "Str";
+        Unit.javaClass->   "Non";
+        else ->            "Any";
+    };
+};
+fun Any.toLong() :Long?{
+    return when(gType(this)){
+        "Byt"-> this.toLong();
+        "Chr"-> this.toLong();
+        "i32"-> this.toLong();
+        "i64"-> null;
+        "Str"-> this.toLong();
+        else->  null;
+    };
+};
